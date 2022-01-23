368321f179ec5d264ec18f92fbad4cf394bb9b43
Martin Di Paola <martinp.dipaola@gmail.com>
2022-01-23 19:57:09 +0000

    Pin some dependencies (pymongo 4.0 was breaking the server)

--- setup.py
+++ setup.py
@@ -15,18 +15,18 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 install_requires = [
     "docker>=2.5.0",
-    "docutils>=0.14",
-    "pymongo>=3.2.2",
-    "PyYAML>=3.11",
+    "docutils>=0.14, <0.18",
+    "pymongo>=3.2.2, <4",
+    "PyYAML>=3.11, <4",
     "web.py==0.40",
-    "lti>=0.9.0",
+    "lti>=0.9.0, <1",
     "oauth2>=1.9.0.post1",
     "httplib2>=0.9",
     "watchdog >= 0.8.3",
     "msgpack >= 1.0.0",
     "pyzmq >= 15.3.0",
     "natsort >= 5.0.1",
-    "psutil >= 4.4.2",
+    "psutil >= 4.4.2, <6",
     "zipstream >= 1.1.4",
     "WsgiDAV == 2.2.4"
 ]
